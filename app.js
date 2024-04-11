cceb38573caf8992a81a857a7cd29bd78c5abfa3
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-04-11 16:06:08 -0300

    feat: adds css file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('pages/index')
