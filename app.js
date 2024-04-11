10f868425f5c874ae7a9082cec043e114f7bdce2
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-04-11 14:06:35 -0300

    chore: installs nodemon

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
+
+const user = {
+  firstName: 'Lucas',
+  lastName: 'Alves',
+}
+
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index', {
+      user: user,
+      admin: true,
+    })
 })
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
