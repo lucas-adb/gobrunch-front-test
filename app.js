4afdf9920220bf6b610d3b533e823f032dc832f7
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-04-11 15:42:11 -0300

    feat: adds index.ejs and basic logic to show items based on user input

--- app.js
+++ app.js
@@ -4,17 +4,8 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
-
-const user = {
-  firstName: 'Lucas',
-  lastName: 'Alves',
-}
-
 app.get('/', (req, res) => {
-    res.render('pages/index', {
-      user: user,
-      admin: true,
-    })
+    res.render('pages/index')
 })
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
